f02073028daccdf75074cb556417327f8ddfc210
Dawid <dawidmartyniuk1@gmail.com>
2025-04-07 22:21:51 +0200

    add login and register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,12 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function register(Request $request)
     {
-      
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|email|unique:users,email',
@@ -25,4 +25,19 @@ class UserController extends Controller
 
         return response()->json($user, 201); 
     }
+    public function login(Request $request){
+
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['error' => 'Invalid email or password'], 401);
+        }
+        
+        return response()->json(['message' => 'Login successful', 'user' => $user]);
+
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('api')->group(function () {
-    
-    Route::post('/users', [UserController::class, 'store']);
+
+    Route::post('/register', [UserController::class, 'register']);
+    Route::post('/login', [UserController::class, 'login']);
 });
